d6371fc8010f8838dfd2ceb92c522701c9f3451f
Yousefanwar004 <yousefanwar004@gmail.com>
2025-12-30 20:25:01 +0200

    updated

--- src/app/Helper/getUserToken.ts
+++ src/app/Helper/getUserToken.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 export async function getUserToken() {
        const myCookies = await cookies()
-       const decodedToken=myCookies.get('next-auth.session-token')?.value ||myCookies.get('__secure-next-auth.session-token')?.value
+       const decodedToken=myCookies.get('next-auth.session-token')?.value ||myCookies.get('__Secure-next-auth.session-token')?.value
      const x=(await cookies()).get('next-auth.session-token')?.value; //bageb token         
      const accessToken= await decode({token:x,secret:process.env.AUTH_SECRET!})    // bafok tashfer cookies
      return accessToken?.token!
